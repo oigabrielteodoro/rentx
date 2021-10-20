32dc99a44dde38b1ee96e14d4f6633c03a02ba5a
Gabriel Teodoro <oi@gabrielteodoro.com>
2021-10-19 23:25:58 -0300

    feat(fonts): installed and configured fonts

--- App.tsx
+++ App.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
 import { NavigationContainer } from '@react-navigation/native'
+import {
+  useFonts,
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_700Bold,
+} from '@expo-google-fonts/archivo'
+import { Inter_400Regular } from '@expo-google-fonts/inter'
+import AppLoading from 'expo-app-loading'
 
 import { Navigation } from '~/Navigation'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_700Bold,
+    Inter_400Regular,
+  })
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return (
     <NavigationContainer>
       <StatusBar style='dark' />
