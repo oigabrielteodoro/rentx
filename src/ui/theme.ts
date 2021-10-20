e8231f167f2821c5843bab2504d6eddbae05f5b8
Gabriel Teodoro <oi@gabrielteodoro.com>
2021-10-19 23:25:25 -0300

    feat(theme): added theme colors and fonts

---
+++ src/ui/theme.ts
@@ -0,0 +1,34 @@
+export const theme = {
+  font: {
+    archivo: {
+      regular: 'Archivo_400Regular',
+      medium: 'Archivo_500Medium',
+      bold: 'Archivo_700Bold',
+    },
+    inter: {
+      regular: 'Inter_400Regular',
+    },
+  },
+  colors: {
+    white: '#ffffff',
+    green: {
+      100: '#daf3e5',
+      500: '#03b352',
+    },
+    red: {
+      100: '#fdedef',
+      300: '#ff99aa',
+      500: '#dc1637',
+    },
+    neutral: {
+      100: '#f4f5f6',
+      200: '#ebebf0',
+      300: '#dedee3',
+      400: '#aeaeb3',
+      500: '#7a7a80',
+      700: '#47474d',
+      800: '#29292e',
+      900: '#1b1b1f',
+    },
+  },
+}
