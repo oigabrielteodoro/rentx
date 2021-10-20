084e01b3bb33156b98e23b9785dddf9c42374e3e
Gabriel Teodoro <oi@gabrielteodoro.com>
2021-10-19 22:36:28 -0300

    chore(pages): add Onboard page

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
 
+import { Onboard } from '~/pages'
+
 export default function App() {
   return (
     <>
       <StatusBar style='dark' />
+      <Onboard />
     </>
   )
 }
---
+++ src/pages/Onboard/Onboard.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { SafeAreaView, Text } from 'react-native'
+
+export function Onboard() {
+  return (
+    <SafeAreaView>
+      <Text>Onboard</Text>
+    </SafeAreaView>
+  )
+}
