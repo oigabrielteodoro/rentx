c96f24a98ed19197c0917cdd35408c7b64458e60
Gabriel Teodoro <oi@gabrielteodoro.com>
2021-12-04 23:29:22 -0300

    chore: add success when account is created

--- src/assets/img/index.ts
+++ src/assets/img/index.ts
@@ -1,5 +1,6 @@
 import SCHEDULE_ICON from './Schedule.png'
 import CAR_ICON from './Car.png'
 import LOGO from './Logo.png'
+import SUCCESS from './Success.png'
 
-export { CAR_ICON, SCHEDULE_ICON, LOGO }
+export { CAR_ICON, SCHEDULE_ICON, LOGO, SUCCESS }
